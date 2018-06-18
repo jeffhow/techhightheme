697c992c925d95aea11b095114859384467547f0
Jeff <jeff@jeff.how>
2018-06-18 12:58:34 -0400

    Add files via upload

--- theme/template-parts/shortcodes/shortcode-card.php
+++ theme/template-parts/shortcodes/shortcode-card.php
@@ -14,8 +14,8 @@
 
 <div class="col-sm">
   <div class="feed-card" style="padding:20px;">
-  <h4><?php the_date(); ?></h4><hr>
-  <small>Daily Announcement</small>
+  <h4><?php the_date(); ?></h4>
+  <small>Daily Announcement</small><hr>
   <div class="text-left"><?php the_excerpt(); ?></div>
   <a href="<?php the_permalink(); ?>" class="btn btn-sm btn-outline-primary">
     Read More &hellip;
@@ -29,11 +29,11 @@
 <div class="col-md-3 col-sm-6">
   <div class="feed-card">
     <div class="d-none d-sm-block feed-card-img" 
-         style="background-image: url('<?php echo get_the_post_thumbnail_url( $page->ID, 'full' ); ?>');">
+         style="background-image: url('<?php echo get_the_post_thumbnail_url( $page->ID, 'medium_large' ); ?>');">
     </div>
     <div class="feed-card-text">
       <div class="feed-card-title"
-               style="background-image: url('<?php echo get_the_post_thumbnail_url( $page->ID, 'full' ); ?>');">
+               style="background-image: url('<?php echo get_the_post_thumbnail_url( $page->ID, 'medium_large' ); ?>');">
         <h3><?php the_title(); ?></h3>
       </div>
       <div class="feed-card-excerpt">
